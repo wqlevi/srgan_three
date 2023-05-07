82acb775bb016a7f498e33fc663121083da72da2
wqlevi <wq.levi@gmail.com>
2023-05-07 20:42:22 +0200

    ddp

--- DDP_train.py
+++ DDP_train.py
@@ -1,22 +1,27 @@
-# TODO
-# -[ ] line 104 error: some parameter in the model was changed inplace; solution: warp all optimization and stuff into function scope
 import torch
+import torch.nn as nn
 from tqdm import tqdm
-import os
+import os, gc
+from datetime import timedelta
 import wandb
 import argparse
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torchvision.models import vgg19
+import torchvision.utils as vutils
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data import random_split
 import numpy as np
 from torch.autograd import Variable
 from dataset import dataloader as DataLoader
-from models.new_model import Generator, Discriminator, VGG19_54
+from models.new_model import Generator, Discriminator, VGG19_54, Discriminator_Unet
+from utils.utils import psnr_cal
 
 def make(opt, rank, world_size):
-    dataset = DataLoader(root=opt.data_path, hr_shape=opt.image_size)
+    n_fraction = 10
+    dataset_all = DataLoader(root=opt.data_path, hr_shape=opt.image_size)
+    dataset,_ = random_split(dataset_all,[len(dataset_all)//n_fraction, len(dataset_all)-len(dataset_all)//n_fraction])
     dist_sampler = DistributedSampler(dataset)
     dataloader = torch.utils.data.DataLoader(dataset, sampler = dist_sampler,  batch_size = opt.batch_size,  num_workers = int(os.cpu_count()/2), drop_last = True)
     return dataloader
@@ -26,22 +31,30 @@ def cleanup():
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+    os.environ['MASTER_PORT'] = '12354'
+    #os.environ['TORCH_CPP_LOG_LEVEL']='INFO'
+    #os.environ['TORCH_DISTRIBUTED_DEBUG'] = 'DETAIL'
     dist.init_process_group('nccl', rank=rank, world_size=world_size)
+    print(f"|Rank-{rank} is initialized|")
 
 def demo(rank, world_size, opt):
     setup(rank, world_size)
+    if dist.get_rank() == 0:
+        print(dist.get_backend())
+        wandb.init(project=opt.model_name,entity='wqlevi')
+        wandb.config.update(opt)
     dataloader = make(opt, rank, world_size)
     Gnet = Generator().to(rank)
-    Dnet = Discriminator((opt.input_channel,opt.image_size,opt.image_size)).to(rank)
+    Dnet_orig = Discriminator((opt.input_channel,opt.image_size,opt.image_size)).to(rank)
+    Dnet= nn.SyncBatchNorm.convert_sync_batchnorm(Dnet_orig)
     FE = VGG19_54(BN=False).to(rank)
 
-    # send each model replica to different GPU process
-    #ddp_Gnet = DDP(Gnet, device_ids=[rank],output_device = rank)
-    #ddp_Dnet = DDP(Dnet, device_ids=[rank],output_device = rank)
-    #ddp_FE = DDP(FE, device_ids=[rank], output_device = rank)
+    #[NOTE]detect anomaly
+    #torch.autograd.set_detect_anomaly(True)
+
     ddp_Gnet, ddp_Dnet, ddp_FE = list(map(lambda x: DDP(x, device_ids=[rank], output_device=[rank]), [Gnet, Dnet, FE]))
 
+    print(f'model inited for rank{rank}')
     Tensor = torch.cuda.FloatTensor
     valid = Variable(Tensor(np.ones((opt.batch_size, *Dnet.output_shape))),requires_grad=False).to(rank)
     fake = Variable(Tensor(np.zeros((opt.batch_size, *Dnet.output_shape))),requires_grad=False).to(rank) 
@@ -58,12 +71,11 @@ def demo(rank, world_size, opt):
     anneal = sigma/opt.num_epochs # decreasing of sigma per epoch
     
     for epoch in tqdm(range(opt.num_epochs),desc='Epochs',colour='green'):
-    #for epoch in range(opt.num_epochs):
         sigma -= anneal
         sigma = max(sigma,0)
         noise_std= torch.full((opt.batch_size,*Dnet.input_shape),sigma,dtype=torch.float32)
         dataloader.sampler.set_epoch(epoch) # shuffle samples
-        for i,imgs in enumerate(dataloader):
+        for i,imgs in enumerate(tqdm(dataloader, desc=f'epoch:{epoch}', colour='white')):
             imgs_hr = imgs['hr'].to(rank)
             imgs_lr = imgs['lr'].to(rank)
             #
@@ -77,7 +89,7 @@ def demo(rank, world_size, opt):
             # MAKE SOME NOISE!!!!
 
             instance_noise = torch.normal(mean=noise_mean, std=noise_std).to(rank)
-            pred_real = ddp_Dnet(imgs_hr+instance_noise).detach() # not computing the gradient wrt this, more restricted function scope needed
+            pred_real = ddp_Dnet(imgs_hr+instance_noise).detach() 
             pred_fake = ddp_Dnet(sr+instance_noise)
             loss_GAN_G = l_BCE(pred_fake - pred_real.mean(0, keepdim=True), valid)
             sr_feature = ddp_FE(sr)
@@ -87,7 +99,6 @@ def demo(rank, world_size, opt):
             
             loss_G = loss_content + 5e-3*loss_GAN_G + 1e-2*loss_pixel
             loss_G.backward() # -[BUG] updating both FE and G?
-            #optimizer_FE.step()
             optimizer_G.step()
             
             #
@@ -95,14 +106,13 @@ def demo(rank, world_size, opt):
             #
             optimizer_D.zero_grad()
             pred_real_d = ddp_Dnet(imgs_hr+instance_noise)
-            pred_fake_d = ddp_Dnet(sr.detach()+instance_noise) # not computing gradient wrt this
+            pred_fake_d = ddp_Dnet(sr.detach()+instance_noise) 
             
             loss_real = l_BCE(pred_real_d - pred_fake_d.mean(0,keepdim=True), valid)
             loss_fake = l_BCE(pred_fake_d - pred_real_d.mean(0,keepdim=True), fake)
             
             loss_D = (loss_real + loss_fake) /2
-            #loss_D.backward(inputs = list(ddp_Dnet.parameters()))
-            loss_D.backward()
+            loss_D.backward() 
             optimizer_D.step()
 
             # 
@@ -111,43 +121,43 @@ def demo(rank, world_size, opt):
             d_sv_list = []
             fe_sv_list= []
             #breakpoint()
-            print('D loss: %.3f'%loss_D.item())
-            with torch.no_grad():
-                for j in range(len(Dnet.model)):
-                    if Dnet.model[j].__str__()[0] == 'N': # meaning its a Norm layer
-                        d_sv_list.append(Dnet.model[j].module.weight_sv.item())
-                for j,m in enumerate(FE.conv_layers.modules()):
-                    if m.__str__()[0] == 'N': # meaning its a Norm layer
-                        fe_sv_list.append(m.module.weight_sv.item())
-                psnr = psnr_cal(sr.cpu().squeeze().numpy(),
-                                imgs_hr.cpu().squeeze().numpy())
-                wandb.log({'train_D_loss':loss_D.item(),
-                           'train_G_loss':loss_G.item(),
-                           'train_psnr':psnr,
-                           'train_SV_Gnet_conv1':Gnet.conv1.module.weight_sv.item(),
-                           'train_SV_Gnet_conv2':Gnet.conv2.module.weight_sv.item(),
-                           'train_SV_Gnet_conv3':Gnet.conv3[0].module.weight_sv.item(),
-                           'train_SV_Dnet_conv1':d_sv_list[0],
-                           'train_SV_Dnet_conv2':d_sv_list[1],
-                           'train_SV_Dnet_conv3':d_sv_list[2],
-                           'train_SV_FE_conv1':fe_sv_list[0],
-                           'train_SV_FE_conv2':fe_sv_list[1],
-                           'train_SV_FE_conv3':fe_sv_list[2]
-                           })
-            if i%opt.plot_per_iter == 0:
-                img_grid = vutils.make_grid(imgs_hr[:4], 2, padding=2, normalize=True).permute(1,2,0)
-                sr_grid = vutils.make_grid(sr[:4], 2,padding=2, normalize=True).permute(1,2,0) # make color channel as last dim
-                #with torch.no_grad():
-                    #y_grad = Grad(sr[0].to(device)) # 1st derivative
-                    #y_lap = laplace(sr[0].cuda()) # 2nd derivative type 1
-                    #y_grad = norm(y_grad)
-                wandb.log({'img':[wandb.Image(img_grid.detach().cpu().numpy(), caption='GT'),
-                                  wandb.Image(sr_grid.detach().cpu().numpy(), caption='SR')
-                                  #wandb.Image(y_grad.permute(1,2,0).detach().cpu().numpy(), caption='grad'),
-                                  #wandb.Image(y_lap.squeeze().permute(1,2,0).detach().cpu().numpy(), caption='laplace')
-                                  ]})
+            if rank == 0:
+                with torch.no_grad():
+                    #for j in range(len(Dnet.model)):
+                    #    if Dnet.model[j].__str__()[0] == 'N': # meaning its a Norm layer
+                    #        d_sv_list.append(Dnet.model[j].module.weight_sv.item())
+                    #for j,m in enumerate(FE.conv_layers.modules()):
+                    #    if m.__str__()[0] == 'N': # meaning its a Norm layer
+                    #        fe_sv_list.append(m.module.weight_sv.item())
+                    psnr = psnr_cal(sr.cpu().squeeze().numpy(),
+                                    imgs_hr.cpu().squeeze().numpy())
+                    wandb.log({'train_D_loss':loss_D.item(),
+                               'train_G_loss':loss_G.item(),
+                               'train_psnr':psnr,
+                    #           'train_SV_Gnet_conv1':Gnet.conv1.module.weight_sv.item(),
+                    #           'train_SV_Gnet_conv2':Gnet.conv2.module.weight_sv.item(),
+                    #           'train_SV_Gnet_conv3':Gnet.conv3[0].module.weight_sv.item(),
+                    #           'train_SV_Dnet_conv1':d_sv_list[0],
+                    #           'train_SV_Dnet_conv2':d_sv_list[1],
+                    #           'train_SV_Dnet_conv3':d_sv_list[2],
+                    #           'train_SV_FE_conv1':fe_sv_list[0],
+                    #           'train_SV_FE_conv2':fe_sv_list[1],
+                    #           'train_SV_FE_conv3':fe_sv_list[2]
+                               })
+                if i%opt.plot_per_iter == 0:
+                    img_grid = vutils.make_grid(imgs_hr[:4], 2, padding=2, normalize=True).permute(1,2,0)
+                    sr_grid = vutils.make_grid(sr[:4], 2,padding=2, normalize=True).permute(1,2,0) # make color channel as last dim
+                    #with torch.no_grad():
+                        #y_grad = Grad(sr[0].to(device)) # 1st derivative
+                        #y_lap = laplace(sr[0].cuda()) # 2nd derivative type 1
+                        #y_grad = norm(y_grad)
+                    wandb.log({'img':[wandb.Image(img_grid.detach().cpu().numpy(), caption='GT'),
+                                      wandb.Image(sr_grid.detach().cpu().numpy(), caption='SR')
+                                      #wandb.Image(y_grad.permute(1,2,0).detach().cpu().numpy(), caption='grad'),
+                                      #wandb.Image(y_lap.squeeze().permute(1,2,0).detach().cpu().numpy(), caption='laplace')
+                                      ]})
             
-            cleanup()
+        cleanup()
         gc.collect()
         torch.cuda.empty_cache()
 
@@ -173,7 +183,5 @@ if __name__ == '__main__':
     parser.add_argument('--multi_gpu',type=int, default=1)
     global opt
     opt = parser.parse_args()
-    wandb.init(project=opt.model_name,entity='wqlevi')
-    wandb.config.update(opt)
-    world_size = 3 # n_gpus
+    world_size = 4 # n_gpus
     run_demo(opt, demo, world_size)
--- debugging/mpi_train.py
+++ debugging/mpi_train.py
@@ -1,5 +1,6 @@
 import torch
 import os
+import wandb
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torchvision.models import vgg19
@@ -8,18 +9,22 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
-    dist.init_process_group('gloo', rank=rank, world_size = world_size)
+    dist.init_process_group('nccl', rank=rank, world_size = world_size)
 
 def cleanup():
     dist.destroy_process_group()
 def demo(rank, world_size):
     #print(f'Running basic DDP on rank {rank}')
     setup(rank, world_size)
+    print(dist.get_backend())
+    if rank == 0:
+        wandb.init(project='test',entity='wqlevi')
     m = vgg19(weights='DEFAULT').to(rank)
     ddp_model = DDP(m, device_ids=[rank])
     output = ddp_model(torch.randn(20,3,64,64))
-    #print(output)
-    #cleanup()
+
+    print(output)
+    cleanup()
 def run_demo(demo_fn, world_size):
     mp.spawn(demo_fn,
              args = (world_size,),
